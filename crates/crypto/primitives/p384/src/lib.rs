4359c7186ba2f74cd8484b7be8bb98c4fe9fdbf7
vanderbr <11340250+vanderbr@users.noreply.github.com>
2026-07-11 11:33:44 +0200

    Fix P-384 and P-521 package verification

--- crates/crypto/primitives/p384/src/lib.rs
+++ crates/crypto/primitives/p384/src/lib.rs
@@ -7,16 +7,25 @@
 #![forbid(unsafe_code)]
 
 mod constants;
+
+#[cfg(feature = "native")]
 mod encoding;
+#[cfg(feature = "native")]
 mod keypair;
+#[cfg(feature = "native")]
 mod sign;
+#[cfg(feature = "native")]
 mod verify;
 
 pub use constants::{
     P384_PUBLIC_KEY_COMPRESSED_LEN, P384_PUBLIC_KEY_RAW_LEN, P384_PUBLIC_KEY_UNCOMPRESSED_LEN,
     P384_SECRET_KEY_LEN, P384_SIGNATURE_DER_MAX_LEN,
 };
+#[cfg(feature = "native")]
 pub use encoding::{compress_p384, decompress_p384};
+#[cfg(feature = "native")]
 pub use keypair::{generate_p384_keypair, generate_p384_keypair_from_secret_key};
+#[cfg(feature = "native")]
 pub use sign::sign_p384_der_prehash;
+#[cfg(feature = "native")]
 pub use verify::verify_p384_der_prehash;
--- crates/crypto/primitives/p521/src/lib.rs
+++ crates/crypto/primitives/p521/src/lib.rs
@@ -7,16 +7,25 @@
 #![forbid(unsafe_code)]
 
 mod constants;
+
+#[cfg(feature = "native")]
 mod encoding;
+#[cfg(feature = "native")]
 mod keypair;
+#[cfg(feature = "native")]
 mod sign;
+#[cfg(feature = "native")]
 mod verify;
 
 pub use constants::{
     P521_PUBLIC_KEY_COMPRESSED_LEN, P521_PUBLIC_KEY_RAW_LEN, P521_PUBLIC_KEY_UNCOMPRESSED_LEN,
     P521_SECRET_KEY_LEN, P521_SIGNATURE_DER_MAX_LEN,
 };
+#[cfg(feature = "native")]
 pub use encoding::{compress_p521, decompress_p521};
+#[cfg(feature = "native")]
 pub use keypair::{generate_p521_keypair, generate_p521_keypair_from_secret_key};
+#[cfg(feature = "native")]
 pub use sign::sign_p521_der_prehash;
+#[cfg(feature = "native")]
 pub use verify::verify_p521_der_prehash;
